010437ef6f488faf557913f2a1f7d4ac3c05abe0
Aitor Sobera <aitorsobera@gmail.com>
2024-04-26 16:55:54 +0200

    refactor: remove boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+
+import { HomeComponent } from '@pages/home/home'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'napame-fe';
 }
