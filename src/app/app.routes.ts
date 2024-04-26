4f581a0c16ee86972b626807823053990858384d
Aitor Sobera <aitorsobera@gmail.com>
2024-04-26 16:56:47 +0200

    feat: add HomeComponent and AdoptionsComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+import { AppComponent } from '@app/app.component'
+import { HomeComponent } from '@pages/home/home'
+import { AdoptionsComponent } from '@pages/adoptions/adoptions'
+
+export const routes: Routes = [
+  {
+	path: '',
+	component: AppComponent,
+    children: [
+      {
+	    path: '',
+	    component: HomeComponent,
+        pathMatch: 'full',
+      },
+      {
+	    path: 'adopciones',
+	    component: AdoptionsComponent,
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  }
+]
